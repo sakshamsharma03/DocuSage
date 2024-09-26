2398b954b2ace6d5038c30c59b1f52cd262d2180
Kaushal12Shinde <kaushushinde12@gmail.com>
2024-09-26 23:59:10 +0530

    fix/  ui, theme change

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -10,15 +10,15 @@ const SideBar = () => {
    
 
     return (
-        <div className={`flex flex-col justify-between h-screen w-[400px] ${theme === 'light' ? 'bg-slate-50 text-black' : 'bg-gray-900 text-white'}`}>
-            <div className='flex   justify-around items-center tracking-tighter   outline-none '>
-                <h1>DocuSage</h1>
+        <div className={`p-[18px] flex flex-col justify-between h-screen w-[400px] ${theme === 'light' ? 'bg-slate-100 text-black' : 'bg-gray-900 text-white'}`}>
+            <div className='flex justify-between items-center tracking-tighter   outline-none '>
+                <h1 className='text-[18px]'>DocuSage</h1>
                 <button onClick={toggleTheme}>
                     {theme === 'light' ? <FaMoon /> : <GoSun />}
                 </button>
             </div>
             <div className='my-20 mx-20'>
-            {/* <PdfReader /> */}
+            <PdfReader />
              </div>
         </div>
     )
